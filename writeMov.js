804fc33ebd15f785039fe5974ef10e72737dc6c8
Sean Kerwin <skerwin@mac.com>
2022-03-10 19:50:46 -0800

    Move more http to https

--- writeMov.js
+++ writeMov.js
@@ -1,8 +1,8 @@
 function writeMov(mlink){
-document.write('<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" height="500" width="640">');
+document.write('<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="https://www.apple.com/qtactivex/qtplugin.cab" height="500" width="640">');
 document.write('<param name="src" value="'+mlink+'">');
 document.write('<param name="autoplay" value="false">');
 document.write('<param name="type" value="video/quicktime" height="500" width="640">');
-document.write('<embed src="'+mlink+'" height="500" width="640" autoplay="false" type="video/quicktime" pluginspage="http://www.apple.com/quicktime/download/">');
+document.write('<embed src="'+mlink+'" height="500" width="640" autoplay="false" type="video/quicktime" pluginspage="https://www.apple.com/quicktime/download/">');
 document.write('</object>');
 }
